ca4eddb5005753410614ae66783e21d0b06b6310
Katios <kate.chonkina@gmail.com>
2021-12-15 08:50:44 +0600

    Initial Commit

---
+++ CardReader/CardReader.h
@@ -0,0 +1,18 @@
+//
+//  CardReader.h
+//  CardReader
+//
+//  Created by Екатерина Чернова on 15.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardReader.
+FOUNDATION_EXPORT double CardReaderVersionNumber;
+
+//! Project version string for CardReader.
+FOUNDATION_EXPORT const unsigned char CardReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardReader/PublicHeader.h>
+
+
